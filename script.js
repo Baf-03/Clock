d5accf6b33b4cd0aadebbde51c38da32ec7b2661
Bilal Ahmed <123253713+Baf-03@users.noreply.github.com>
2023-04-06 22:41:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ setInterval(()=>{
 	s = d.getSeconds();
 
 	//--------------------------------------rotation-of-hands-----------------------------
-
 	hour_rotation = 30*h + m/2;
 	minutes_rotation = 6*m;
 	seconds_rotation = 6*s
@@ -14,42 +13,33 @@ setInterval(()=>{
 	minute.style.transform = `rotate(${minutes_rotation}deg)`;
 	second.style.transform = `rotate(${seconds_rotation}deg)`;
 },1000)
-let dateTime = new Date();
-let day = dateTime.getDay();
-let date = dateTime.getDate();
-let month = dateTime.getMonth();
-let year = dateTime.getFullYear();
+//where d is date
+let day = d.getDay();
+let date = d.getDate();
+let month = d.getMonth();
+let year = d.getFullYear();
+
 let monthArr = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];//this is done to display short names of month instead of numbers
 let dayArr = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
+
 let presentDay = `${dayArr[day]}`;
 let presentDate = `${date} ${monthArr[month]} ${year}`
+
 document.getElementById('present-day').innerText = presentDay;
 document.getElementById('date').innerText = presentDate;
 
-
-
 //Digital Clock
 function time() {
-
-    
-
     let meridian = 'AM';
-  
     if (h >= 12) {
         h = h - 12;
         meridian = 'PM';
     }
-
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
 
     let presentTime = `${h}: ${m}: ${s} ${(meridian)}`;
-    
-
     document.getElementById('display-clock').innerText = presentTime;
-    
 }
-
-
 setInterval(time, 1000);
