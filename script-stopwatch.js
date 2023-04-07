b41d9da31ef30a0369f68783fc6854bb823c3aca
Bilal Ahmed <123253713+Baf-03@users.noreply.github.com>
2023-04-07 09:21:09 +0500

    Add files via upload

---
+++ script-stopwatch.js
@@ -0,0 +1,101 @@
+let x=document.getElementById("output");
+let y=document.getElementById("laps");
+
+let pause=true;//it means that the stop_watch is stop rn
+let minutes=0;
+let hours=0;
+let seconds=0;
+let ms=0;
+let lap_counter=0;
+
+
+function start_timer(){
+	if (pause== true){
+	pause = false;
+	timer();
+	};
+};
+
+function timer(){
+	
+	if(pause == false){
+		ms=parseInt(ms)
+		minutes=parseInt(minutes);
+		seconds=parseInt(seconds);
+		hours=parseInt(hours);	
+		ms+=1
+		
+		if(ms==1000){
+			seconds+=1;
+			ms=0;
+		}
+		if(seconds==60){
+			seconds=0;
+			minutes+=1;
+		}
+		if(minutes==60){
+			minutes=0;
+			hours+=1
+		}
+		if (ms<10){
+			ms = "0"+"0"+ms;
+		}else if (ms<100){
+			ms = "0"+ms;
+		}
+		
+		if(hours < 10){
+			hours= "0"+ hours;
+
+		}
+
+		if(minutes < 10){
+			minutes = "0"+ minutes;
+		
+		}
+
+		if(seconds < 10){
+			seconds= "0"+ seconds;
+		};
+
+		
+
+		x.innerHTML = hours +":"+minutes+":"+seconds+":"+ms;
+		setTimeout("timer()",1);//agr yae nahi lagata tou loop nahiahi 
+								// banta agr loop nahi banta tou bs ek bar chal kr ruk jata take 
+								// yae chalta rahe jab tk condition false na hojae hum nae yae use kia iss ko python mae hum recursion kehtae thae
+
+	}
+}
+
+function record_lap() {if (true) {}
+  			lap_counter++;
+  			let lap_time = hours + ":" + minutes + ":" + seconds + ":" + ms;
+  			let new_lap = document.createElement("div");
+  			new_lap.innerHTML = "Lap " + lap_counter + ": " + lap_time;
+  			y.appendChild(new_lap);
+}
+function stop_timer(){
+	if(pause==false){
+	pause=true;
+
+}
+}
+
+function reset_timer(){
+	x.innerHTML = "00:00:00:00";
+	y.innerHTML = "";
+	pause = true;
+	hours = 0;
+	seconds = 0;
+	minutes = 0;
+	ms=0;
+	lap_counter=0;
+
+}
+
+
+
+// start.addEventListener('click',start_timer());
+// start.addEventListener('click',stop_timer());
+// reset.addEventListener('click',reset_timer());
+
